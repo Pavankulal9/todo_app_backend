a5e569a767593c0716fc78c90718c472617f8df3
Pavan <pawankulal9@gmail.com>
2024-12-06 12:05:39 +0530

    testing of ssl certificate in aws ec2 machine

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-
+import https from "https"; 
+import fs from "fs"; 
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
+  path: './.env'
 });
+
+const options = {
+  key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
+  cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+};
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server Connected to PORT:${process.env.PORT}`);
+  .then(() => {
+    // Start the HTTPS server
+    https.createServer(options, app).listen(process.env.PORT, () => {
+      console.log(`Server connected to PORT: ${process.env.PORT} and running on HTTPS`);
     });
-});
-
-
-
-
-
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
