c91292dbea4f246dd746bc01308b905c290607dd
Pavan <pawankulal9@gmail.com>
2026-07-10 11:39:49 +0530

    refactor :recycle: Refactor the code to not include ssl file access for render deploy service.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-import https from "https"; 
-import fs from "fs"; 
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: './.env'
+  path: "./.env",
 });
-
-const options = {
-  key: fs.readFileSync("/home/ubuntu/ssl/selfsigned.key"),
-  cert: fs.readFileSync("/home/ubuntu/ssl/selfsigned.crt"),
-};
 
 connectDB()
   .then(() => {
     // Start the HTTPS server
-    https.createServer(options, app).listen(process.env.PORT, () => {
-      console.log(`Server connected to PORT: ${process.env.PORT} and running on HTTPS`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Connected to PORT:${process.env.PORT}`);
     });
   })
   .catch((err) => {
